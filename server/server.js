393e565709f164b67de5008c440bef2a7473b6e9
Troy Crawford <tacrawford91@gmail.com>
2018-05-20 15:45:16 -0500

    Created message text, and generate message utility

--- server/server.js
+++ server/server.js
@@ -6,6 +6,7 @@ var PORT = process.env.PORT || 3000;
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+const {generateMessage} = require('./utils/message')
 const http = require('http');
 var server = http.createServer(app); 
 const socketIO = require('socket.io');
@@ -18,25 +19,13 @@ app.use(express.static(publicPath));
 io.on('connection',(socket)=> {
     console.log('new connection');
     
-    socket.emit('newMessage', {
-        from: 'Admin',
-        text: 'Welcome to the chat App',
-        createdAt: new Date().getTime()
-    })
+    socket.emit('newMessage', generateMessage('Admin', 'Welcome to nodeChatter'));
 
-    socket.broadcast.emit('newMessage', {
-        from: 'Admin',
-        text: 'New User Entered in the room',
-        createdAt: new Date().getTime()
-    })    
+    socket.broadcast.emit('newMessage', generateMessage('Admin', 'New User Joined'));  
 
     socket.on('createMessage', (message)=>{
         console.log('create Message', message);
-        io.emit('newMessage', {
-            from: message.from,
-            text: message.text,
-            createdAt: new Date().getTime()
-        });
+        io.emit('newMessage', generateMessage(message.from, message.text));
         // socket.broadcast.emit('newMessage',{
         //     from: message.from,
         //     text: message.text,
