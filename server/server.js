42dbca5c49e56d70f95aac8bdb1e718c950dc30c
Troy Crawford <tacrawford91@gmail.com>
2018-05-20 12:13:44 -0500

    intial file setup

---
+++ server/server.js
@@ -0,0 +1,15 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+var path = require("path");
+var app = express();
+var PORT = process.env.PORT || 3000;
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+const publicPath = path.join(__dirname,'../public');
+
+app.use(express.static(publicPath));
+
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+});
