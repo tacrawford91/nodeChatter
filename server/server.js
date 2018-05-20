3420a875ce51c0852e429187bd2ff74c19c8ff33
Troy Crawford <tacrawford91@gmail.com>
2018-05-20 12:51:38 -0500

    intial socket setup and heroku built

--- server/server.js
+++ server/server.js
@@ -6,10 +6,24 @@ var PORT = process.env.PORT || 3000;
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+const http = require('http');
+var server = http.createServer(app); 
+const socketIO = require('socket.io');
+var io = socketIO(server);
 const publicPath = path.join(__dirname,'../public');
 
 app.use(express.static(publicPath));
 
-app.listen(PORT, () => {
+
+io.on('connection',(socket)=> {
+    console.log('new connection');
+
+    socket.on('disconnect', ()=>{
+        console.log('Client Disconnected');
+    })
+})
+
+
+server.listen(PORT, () => {
     console.log("App listening on PORT " + PORT);
 });
